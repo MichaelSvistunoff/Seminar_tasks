925b8d9aeaa8bc0dfd299a8438792f30a93862ab
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-20 06:01:39 +0600

    Second task

---
+++ 2_Seminar_2_task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программу, которая выводит трехзначное число и удаляет вторую цифру этого числа
+
+int number = new Random().Next(100, 1000);
+
+int numOne = number/100;
+int numTwo = number%10;
+int numThree = numOne*10 + numTwo;
+
+Console.WriteLine(number);
+Console.WriteLine(numThree);
