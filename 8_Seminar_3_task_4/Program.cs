aeff4d34ccb9d9e0e083ae0c1da18847e57076a4
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-23 06:07:44 +0600

    Зачада на вывод всех четных чисел от 1 до N с циклом for

---
+++ 8_Seminar_3_task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу, которая на вход принимает число N,
+а на выходе показывает все четные числа от 1 до N
+*/
+
+Console.WriteLine("Enter number (>0): ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++) {
+    if (i%2==0){
+        Console.Write($"{i} ");
+    }
+}
