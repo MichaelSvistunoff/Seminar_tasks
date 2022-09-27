0ef08c3f40e890d2411afb85dcd579f2796af3b1
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-28 03:27:56 +0600

    Задача на произведение цифр в числе

---
+++ 13_Seminar_4_task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает на вхож число N и выдает произведение от 1 до N
+4-24
+5-120
+*/
+
+Console.WriteLine("Enter your number: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"The multiplication of your number {N} = {GetMult(N)}");
+
+int GetMult(int final) {
+    int multiplication = 1;
+    for (int i=2; i<=final; i++) {
+        multiplication=multiplication*i;
+    }
+    return multiplication;
+}
