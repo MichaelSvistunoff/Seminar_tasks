a62ed0edba9ef4a5237590f7c0dba255b68f5288
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-28 04:05:44 +0600

    Задача на заполнение массива (рандом)

---
+++ 14_Seminar_4_task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая выводит массив из 8 элементов, заполненный 0 и 1 в случайном порядке
+*/
+
+int [] ar = fillArray(8);
+Console.WriteLine($"[{String.Join(", ", ar)}]");
+// printArray(ar);
+
+int [] fillArray(int size) {
+    int [] array = new int[size];
+    for (int i=0; i<size; i++) {
+        array [i] = new Random().Next(0,2);
+    }
+    return array;
+}
+
+// void printArray(int [] arr){
+//     for (int i=0; i<arr.Length; i++) {
+//         Console.Write($"{arr[i]} ");
+//     }
+// }
