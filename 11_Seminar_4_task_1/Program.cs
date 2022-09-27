95313169ff7369e1db6d4e288d525bf41efe73c3
MichaelSv <michaelsvistunoff@gmail.com>
2022-09-28 02:04:05 +0600

    Задача на вывод суммы от 1 до А

---
+++ 11_Seminar_4_task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 
+Напишите программу, которая принимает на вход число А и выдает
+сумму чисел от 1 до А
+
+7 - 28
+4 - 10
+8 - 36
+*/
+
+Console.WriteLine("Enter Number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Sum from 1 to {number} = {GetSum(number)}");
+
+int GetSum(int limit) {
+    int sum = 0;
+    for (int i = 1; i <= limit; i++) {
+        sum = sum + i;
+    }
+    return sum;
+}
