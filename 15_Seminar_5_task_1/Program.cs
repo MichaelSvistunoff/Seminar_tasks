e756d5e621a314fb4037f40555ca2d0b2496dedd
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-01 23:38:16 +0600

    Сумма положительных и отрицательных элементов массива

---
+++ 15_Seminar_5_task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9; 9].
+Найти сумму положительных и отрицательных элементов массива
+*/
+
+// 2. Вызов массива
+int [] array = getArray(12, -9, 9);
+Console.WriteLine(string.Join(" ", array));
+
+// 3. Введение держателей сумм (+/-)
+int positiveSum = 0;
+int negativeSum = 0;
+
+// 4. Выборка по положительным и отрицательным элементам
+for (int i=0; i<array.Length; i++) {
+    if (array[i]<0){
+        negativeSum += array[i];
+    } else {
+        positiveSum += array[i];
+    }
+}
+
+Console.WriteLine($"Сумма положительных: {positiveSum}. Сумма отрицательных: {negativeSum}");
+
+// 1. Создание метода массива по условиям задачи
+int[] getArray(int size, int minValue, int maxValue) {
+    int[] newArray = new int[size];
+    for (int i=0; i<size; i++) {
+        newArray[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return newArray;
+}
