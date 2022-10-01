eb6fb3bd80e3827542d5e0661e13a492a6fb4b66
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-02 00:27:28 +0600

    Задача на генерацию массива и замену знаков в элементах массива

---
+++ 16_Seminar_5_task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите задачу по замене элементов массива с положительных на отрицательные и наоборот
+*/
+
+// Выозов массива
+int[] array = getArray(4, -5, 5);
+Console.WriteLine($"Массив оригинал: [{String.Join(" ", array)}]");
+
+for (int i=0; i<array.Length; i++) {
+    array[i] = array[i]*-1;
+}
+Console.WriteLine($"Массив после: [{String.Join(" ", array)}]");
+
+// Создание метода генерации массива с заполнением
+int[] getArray (int size, int startArray, int endArray) {
+    int[] array = new int[size];
+
+    for (int i=0; i<size; i++) {
+        array[i] = new Random().Next(startArray, endArray+1);
+    }
+    return array;
+}
