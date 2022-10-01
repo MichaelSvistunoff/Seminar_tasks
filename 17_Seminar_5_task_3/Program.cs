9dd29c20c5c31d1aa370ec672fb7ba2a6ab44db9
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-02 01:35:13 +0600

    Задача на поиск элемента массива

---
+++ 17_Seminar_5_task_3/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задать массив. Написать программу, которая определяет присутствует ли данное число в массиве
+*/
+
+int[] newArray = getArray(10);
+Console.WriteLine($"[{String.Join(", ", newArray)}]");
+
+Console.WriteLine("Введите число, которое нужно найти в массиве: ");
+int findNumber = int.Parse(Console.ReadLine()!);
+
+int aim = ifElemIsFound(newArray, findNumber);
+switch (aim) {
+    case -1:
+        Console.WriteLine("Жаль! Твоего числа нет в массиве");
+        break;
+    default:
+        Console.WriteLine($"Круто! Твоё число с индексом {aim}");
+        break;
+}
+
+int[] getArray(int size) {
+    int[] array = new int[size];
+
+    for (int i=0; i<size; i++) {
+        array[i] = new Random().Next(-10, 11);
+    }
+    return array;
+}
+
+int ifElemIsFound (int[] array, int find) {
+    int position = -1;
+
+    for (int i=0; i<array.Length; i++) {
+        if (array[i]==find) {
+            position=i;
+            break;
+        }
+    }
+    return position;
+}
