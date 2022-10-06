555e2bfc285f6001b132c37a425cc625596714df
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-07 05:21:09 +0600

    Задача на создание и заполнение двумерного массива размером M*N

---
+++ 25_Seminar_7_task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Создайте двумерный массив M*N
+Заполните рандомными числами
+*/
+
+Console.WriteLine("Enter number of rows of array ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter number of columns of array ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = getArray(rows, cols);
+printArray(array);
+
+int[,] getArray(int m, int n) {
+    int[,] newArray = new int[m, n];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            newArray[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return newArray;
+}
+
+void printArray (int[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
