d387be8543e2fbd3071a808d0151880e7bba346c
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-16 03:13:18 +0600

    Задача на вывод чисел от M до N через рекурсию

---
+++ 34_Seminar_9_task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Написать программу, кторая покажет все числа от M до N.
+Через рекурсию
+*/
+
+Console.WriteLine("Enter your num: ");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter your num: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(RecursionN(M, N));
+
+string RecursionN (int m, int number) {
+    if (m == number) {
+        return number.ToString();
+    }
+    return (m + " " + RecursionN(m+1, number));
+}
