715d7c2db23fcce5489a53bf2e0a7d57fde48744
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-16 03:05:07 +0600

    Задача на вывод чисел от 1 до N через рекурсию

---
+++ 33_Seminar_9_task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Написать программу, кторая покажет все числа от 1 до N.
+Через рекурсию
+*/
+
+Console.WriteLine("Enter your num: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(RecursionN(1, n));
+
+string RecursionN (int minVal, int number) {
+    if (minVal == number) {
+        return number.ToString();
+    }
+    return (minVal + " " + RecursionN(minVal+1, n));
+}
