13fbf32945e7d5abb459364da5075c0f00472f87
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-16 03:57:02 +0600

    Задача на возведение числа в степень через рекурсию

---
+++ 36_Seminar_9_task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Написать программу, которая на вход принимает 2 числа A и B.
+И возводит число A в целую степень B через рекурсию
+
+A = 3, B = 5 => 243
+A = 2, B = 3 => 8
+*/
+
+Console.WriteLine("Enter A: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter A: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetResultOfAInB(A, b));
+
+int GetResultOfAInB (int number, int extent) {
+    if (extent == 0) return 1;
+    else if (extent == 1) return number;
+    return number * GetResultOfAInB(number, extent-1);
+}
