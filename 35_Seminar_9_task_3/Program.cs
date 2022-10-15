ab46b7bae0124b1d0666f6c2df67ebedf8fc60ed
MichaelSv <michaelsvistunoff@gmail.com>
2022-10-16 03:35:11 +0600

    Задача на сумму цифр числа через рекурсию

---
+++ 35_Seminar_9_task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая будет принимать на вход число и будет возвращать сумму его цифр
+
+453 - 12
+*/
+
+Console.WriteLine("Enter your number: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetSum(N));
+
+int GetSum(int number) {
+    if(number==0) {
+        return 0;
+    }
+    return (number%10 + GetSum(number/10));
+}
